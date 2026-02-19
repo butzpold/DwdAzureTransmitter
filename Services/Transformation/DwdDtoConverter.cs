cb196600c9a2da50fa1bdde0560eeb17564228a3
butzpold <leopold.bachmann@web.de>
2026-02-19 17:54:42 +0100

    renaming namespace according to project name; adding a feature for preventing from invalid timestamps to ConvertUnixMsToTime()

--- Services/Transformation/DwdDtoConverter.cs
+++ Services/Transformation/DwdDtoConverter.cs
@@ -1,17 +1,39 @@
-﻿using ApiJsonSqlServer.Domain;
-using ApiJsonSqlServer.Models;
+﻿using DwdAzureSqlDataTransmitter.Domain;
+using DwdAzureSqlDataTransmitter.Models;
 
-namespace ApiJsonSqlServer.Services
+namespace DwdAzureSqlDataTransmitter.Services
 {
     internal class DwdDtoConverter
     {
-        private static TimeOnly ConvertUnixMsToTime(long unixMs) =>
-            TimeOnly.FromDateTime(
-                DateTimeOffset
-                    .FromUnixTimeMilliseconds(unixMs)
+        private static TimeOnly? ConvertUnixMsToTime(long? unixMs)
+        {
+            // if API sends a negative/null-value timestamp
+            if (unixMs == null || unixMs <= 0)
+                return null;           
+            try
+            {
+                var dateTime = DateTimeOffset
+                    // intervenes together with try/catch
+                    // if the timestamp isn't a valid number;
+                    // mostly insane large (9999999999999999999)
+                    .FromUnixTimeMilliseconds(unixMs.Value)
                     .ToLocalTime()
-                    .DateTime
-                );
+                    .DateTime;
+
+                var time = dateTime.TimeOfDay;
+                // if timespan is invalid; has to be between TimeSpan.Zero (=00:00:00) &
+                // TimeSpan.FromDays(1) (=23:59:59.9999999)
+                if (time < TimeSpan.Zero || time >= TimeSpan.FromDays(1))
+                    return null;
+
+                return TimeOnly.FromTimeSpan(time);
+            }
+            catch
+            {
+                // If API sends completely broken timestamp
+                return null;
+            }
+        }
         private static double ScaleToDouble(int value) => value / 10.0;
         private static int ScaleToInt(int value) 
             => (int)Math.Round(ScaleToDouble(value), MidpointRounding.AwayFromZero);
